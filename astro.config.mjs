fac7ada64dccb17fa0423c695ff7eed929674835
mainura97 <mainuramahasir@gmail.com>
2024-10-26 12:32:46 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mainura97.github.io'
+    site: 'https://whatdev.github.io'
 });
